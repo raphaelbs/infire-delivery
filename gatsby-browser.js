23517c9d953796ad0dcd81edcb81759222949577
Raphael Brandão <raphael.brandao@quintoandar.com.br>
2020-02-09 23:39:55 -0300

    add pwa tracking

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,5 @@
+const trackCustomEvent = require('gatsby-plugin-google-analytics');
+
 /**
  * Implement Gatsby's Browser APIs in this file.
  *
@@ -5,3 +7,14 @@
  */
 
 // You can delete this file if you're not using it
+exports.onClientEntry = () => {
+  window.addEventListener('beforeinstallprompt', e => {
+    e.userChoice.then(choiceResult => {
+      trackCustomEvent({
+        category: 'add-to-homescreen',
+        action: 'prompt',
+        value: choiceResult.outcome,
+      })
+    });
+  });
+}
