7ccdbe3b5c7348835ed4c3eaa70bbde87e758b00
Raphael Brandão <raphael.b.souza@hotmail.com>
2020-02-15 16:48:37 -0300

    disable identity widget

--- gatsby-config.js
+++ gatsby-config.js
@@ -109,7 +109,7 @@ module.exports = {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
-        // enableIdentityWidget: false,
+        enableIdentityWidget: false,
       },
     },
     `gatsby-plugin-preact`,
