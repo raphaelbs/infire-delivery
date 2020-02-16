33e50e6723f1160f460b7696dff9e7f5ff473c78
Raphael Brandão <raphael.brandao@quintoandar.com.br>
2020-02-16 18:03:38 -0300

    update pedir text

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const WHATSAPP_URL = "https://wa.me/5537988329573";
 export const INSTAGRAM_URL = "https://www.instagram.com/infiredelivery";
 export const PEDIR_TEXT = items =>
-  encodeURI(`Olá! Segue meu pedido: ${items.map(({ qtd, title, index }) => `${index ? ',' : ''}${qtd}x do ${title}`)}.`);
+  encodeURI(`Olá! Segue meu pedido: ${items.map(({ qtd, title, index }) => `${index ? ', ' : ''}${qtd}x - ${title}`)}.`);
 
 export const displayPrice = value => `R$ ${(value.toFixed(2) + "").replace(".", ",")}`;
