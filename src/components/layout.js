26d7c7038a885aec5370c8fbc82100ff7cba4944
Raphael Brandão <raphael.brandao@quintoandar.com.br>
2020-02-17 12:33:29 -0300

    read data from seo

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
@@ -15,15 +8,16 @@ import Header from "./header";
 
 const Layout = ({ children, subtitle }) => {
   const data = useStaticQuery(siteMetadata);
+  const seo = data.pageData.edges[0].node.frontmatter;
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} subtitle={subtitle} />
+      <Header siteTitle={seo.title} subtitle={subtitle} />
       <div
         style={{
           margin: `0 auto`,
           maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
+          padding: `0px 1rem 1rem`,
           paddingTop: 0,
         }}
       >
@@ -41,10 +35,14 @@ Layout.propTypes = {
 export default withTheme(Layout);
 
 const siteMetadata = graphql`
-query SiteTitleQuery {
-  site {
-    siteMetadata {
-      title
+query {
+  pageData: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/seo\\\\.md$/"}}) {
+    edges {
+      node {
+        frontmatter {
+          title
+        }
+      }
     }
   }
 }
