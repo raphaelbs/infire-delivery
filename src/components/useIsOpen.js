0a8cb453488a97ce3c9b4c4bc9ffa3b3b5bf0dbd
Raphael Brandão <raphael.brandao@quintoandar.com.br>
2020-02-23 16:04:42 -0300

    add useIsOpen hook

---
+++ src/components/useIsOpen.js
@@ -0,0 +1,64 @@
+import { graphql, useStaticQuery } from "gatsby";
+import { useState, useEffect } from "react";
+
+const useIsOpen = () => {
+  const data = useStaticQuery(statusMetadata);
+  const [isOpen, setOpen] = useState(false);
+
+  const statusData = data.status.edges[0].node.frontmatter;
+
+  const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thrusday', 'friday', 'saturday']
+    .map(day => statusData[day]);
+  const [startHour, startMinute] = statusData.startHour.split(':').map((v) => parseInt(v));
+  const [endHour, endMinute] = statusData.endHour.split(':').map((v) => parseInt(v));
+  const startDate = new Date(0, 0, 0, startHour, startMinute);
+  const endDate = new Date(0, 0, 0, endHour, endMinute);
+
+  const intervalFn = () => {
+    const now = new Date();
+    const currentDay = now.getDay();
+    const currentHour = now.getHours();
+    const currentMinute = now.getMinutes();
+
+    const currentDate = new Date(0, 0, 0, currentHour, currentMinute);
+  
+    const isDayAllowed = days[currentDay];
+    const isHourAllowed = startDate <= currentDate && currentDate <= endDate;
+  
+    setOpen(isDayAllowed && isHourAllowed);
+  };
+
+  useEffect(() => {
+    intervalFn();
+    const interval = setInterval(intervalFn, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return [isOpen];
+};
+
+export default useIsOpen;
+
+const statusMetadata = graphql`
+query {
+  status: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/status\\\\.md$/"}}) {
+    edges {
+      node {
+        frontmatter {
+          startHour
+          endHour
+          monday
+          tuesday
+          wednesday
+          thrusday
+          friday
+          saturday
+          sunday
+        }
+      }
+    }
+  }
+}
+`;
+
