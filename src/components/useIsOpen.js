c262fa56c90dfbf18a1a0d4df4dbbc4ff5acb4e2
Raphael Brandão <raphael.brandao@quintoandar.com.br>
2020-02-23 16:35:52 -0300

    try improve perf

--- src/components/useIsOpen.js
+++ src/components/useIsOpen.js
@@ -4,36 +4,36 @@ import { useState, useEffect } from "react";
 const useIsOpen = () => {
   const data = useStaticQuery(statusMetadata);
   const [isOpen, setOpen] = useState(false);
-
-  const statusData = data.status.edges[0].node.frontmatter;
-
-  const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thrusday', 'friday', 'saturday']
-    .map(day => statusData[day]);
-  const [startHour, startMinute] = statusData.startHour.split(':').map((v) => parseInt(v));
-  const [endHour, endMinute] = statusData.endHour.split(':').map((v) => parseInt(v));
-  const startDate = new Date(0, 0, 0, startHour, startMinute);
-  const endDate = new Date(0, 0, 0, endHour, endMinute);
-
-  const intervalFn = () => {
-    const now = new Date();
-    const currentDay = now.getDay();
-    const currentHour = now.getHours();
-    const currentMinute = now.getMinutes();
-
-    const currentDate = new Date(0, 0, 0, currentHour, currentMinute);
   
-    const isDayAllowed = days[currentDay];
-    const isHourAllowed = startDate <= currentDate && currentDate <= endDate;
-  
-    setOpen(isDayAllowed && isHourAllowed);
-  };
-
   useEffect(() => {
+    const statusData = data.status.edges[0].node.frontmatter;
+  
+    const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thrusday', 'friday', 'saturday']
+      .map(day => statusData[day]);
+    const [startHour, startMinute] = statusData.startHour.split(':').map((v) => parseInt(v));
+    const [endHour, endMinute] = statusData.endHour.split(':').map((v) => parseInt(v));
+    const startDate = new Date(0, 0, 0, startHour, startMinute);
+    const endDate = new Date(0, 0, 0, endHour, endMinute);
+
+    const intervalFn = () => {
+
+      const now = new Date();
+      const currentDay = now.getDay();
+      const currentHour = now.getHours();
+      const currentMinute = now.getMinutes();
+  
+      const currentDate = new Date(0, 0, 0, currentHour, currentMinute);
+    
+      const isDayAllowed = days[currentDay];
+      const isHourAllowed = startDate <= currentDate && currentDate <= endDate;
+    
+      setOpen(isDayAllowed && isHourAllowed);
+    };
     intervalFn();
-    const interval = setInterval(intervalFn, 1000);
+    const interval = setInterval(() => requestAnimationFrame(intervalFn), 20000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [data]);
 
   return [isOpen];
 };
