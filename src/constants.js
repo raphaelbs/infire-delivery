b7c26db544374c7552b5f55c2e8df08b9375be99
Raphael Brandão <raphael.brandao@quintoandar.com.br>
2020-05-23 14:49:23 -0300

    change links from button to href

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,9 @@
-export const WHATSAPP_URL = 'https://wa.me/5537988329573';
-export const INSTAGRAM_URL = 'https://www.instagram.com/infiredelivery';
+export const INSTAGRAM_NAME = 'infiredelivery';
+export const PHONE_NUMBER = '(37) 98832-9573';
+
+export const INSTAGRAM_URL = `https://www.instagram.com/${INSTAGRAM_NAME}`;
+export const WHATSAPP_URL = `https://wa.me/55${PHONE_NUMBER.replace(/[^\d]/g, '')}`;
+
 export const PEDIR_TEXT = items =>
   encodeURI(`Olá!\n\nSegue meu pedido:\n${items.map(({ qtd, title }) => `${qtd}x ${title}`).join(',\n')}`);
 
