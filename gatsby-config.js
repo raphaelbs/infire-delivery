8ee3b57a95113938f12e05f8788194231c95f591
Raphael Brandão <raphael.brandao@quintoandar.com.br>
2020-12-17 19:48:48 -0300

    enable identify widget

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,6 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
-        enableIdentityWidget: false,
       },
     },
     {
